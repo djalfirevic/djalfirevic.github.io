dc8ab1d383df86eaaf55b997e1b421fd9cba1411
Djuro Alfirevic <djalfirevic@gmail.com>
2020-12-02 08:25:59 +0100

    Add Level Home app

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,171 +1,180 @@
 const apps = [
   {
-    name: "Skippit",
-    image: "skippit.png",
-    appStoreLink: "https://apps.apple.com/us/app/skippit/id1475092571",
-    appStoreImages: ["skippit-1.png", "skippit-2.png"],
-    features: ["Twillio", "Image Filtering", "Test-Driven Development"],
+    name: 'Level Home',
+    image: 'level-home.png',
+    appStoreLink: 'https://apps.apple.com/us/app/level-home/id1490027518',
+    appStoreImages: ['level-home-1.png', 'level-home-2.png'],
+    features: ['Bluetooth', 'Test-Driven Development'],
     description:
-      "Skippit is a new type of first date that uses video calling to replace the expensive Ubers, awkward first drinks, and cat-fishing that’s common in most dating apps.",
+      'Level is creating the next generation of smart locks with the Level Bolt and Level Touch. Level Bolt is the world’s first invisible smart lock that installs inside your door to make your current lock smart.',
   },
   {
-    name: "Celsius",
-    image: "celsius.png",
-    appStoreLink:
-      "https://apps.apple.com/us/app/celsius-network-crypto-wallet/id1387885523",
-    appStoreImages: ["celsius-1.png", "celsius-2.png"],
-    features: ["React Native", "Onfido SDK", "Test-Driven Development"],
+    name: 'Skippit',
+    image: 'skippit.png',
+    appStoreLink: 'https://apps.apple.com/us/app/skippit/id1475092571',
+    appStoreImages: ['skippit-1.png', 'skippit-2.png'],
+    features: ['Twillio', 'Image Filtering', 'Test-Driven Development'],
     description:
-      "Calling all HODLers! Would you like to earn interest using 26 different crypto currencies in one crypto wallet? Then Celsius Network Wallet is the perfect crypto app for you! Stay on top of community events & cryptocurrency news, while you deposit and earn interest rewards (up to 11% APY) on CEL, BTC, ETH, and other crypto. Automatically qualify for a 1% APR dollar loan using your crypto as collateral! And the best part - Never pay any fees!",
+      'Skippit is a new type of first date that uses video calling to replace the expensive Ubers, awkward first drinks, and cat-fishing that’s common in most dating apps.',
   },
   {
-    name: "Bitcoin Cash Register",
-    image: "bitcoin-cash-register.png",
+    name: 'Celsius',
+    image: 'celsius.png',
     appStoreLink:
-      "https://apps.apple.com/us/app/bitcoin-cash-register-bch/id1460652887",
+      'https://apps.apple.com/us/app/celsius-network-crypto-wallet/id1387885523',
+    appStoreImages: ['celsius-1.png', 'celsius-2.png'],
+    features: ['React Native', 'Onfido SDK', 'Test-Driven Development'],
+    description:
+      'Calling all HODLers! Would you like to earn interest using 26 different crypto currencies in one crypto wallet? Then Celsius Network Wallet is the perfect crypto app for you! Stay on top of community events & cryptocurrency news, while you deposit and earn interest rewards (up to 11% APY) on CEL, BTC, ETH, and other crypto. Automatically qualify for a 1% APR dollar loan using your crypto as collateral! And the best part - Never pay any fees!',
+  },
+  {
+    name: 'Bitcoin Cash Register',
+    image: 'bitcoin-cash-register.png',
+    appStoreLink:
+      'https://apps.apple.com/us/app/bitcoin-cash-register-bch/id1460652887',
     appStoreImages: [
-      "bitcoin-cash-register-1.png",
-      "bitcoin-cash-register-2.png",
+      'bitcoin-cash-register-1.png',
+      'bitcoin-cash-register-2.png',
     ],
-    features: ["Blockchain", "BIP70", "Test-Driven Development"],
+    features: ['Blockchain', 'BIP70', 'Test-Driven Development'],
     description:
-      "This simple Point of Sale allows you to accept Bitcoin Cash (BCH) payments at any retail location. All it requires is an address linked to your wallet to start receiving Bitcoin Cash (BCH) payments.",
+      'This simple Point of Sale allows you to accept Bitcoin Cash (BCH) payments at any retail location. All it requires is an address linked to your wallet to start receiving Bitcoin Cash (BCH) payments.',
   },
   {
-    name: "Metoro VPN",
-    image: "metoro.png",
-    appStoreLink: "https://apps.apple.com/us/app/metoro-vpn/id1495349422?ls=1",
-    appStoreImages: ["metoro-1.png", "metoro-2.png"],
-    features: ["VPN", "OpenVPN", "IKEv2"],
+    name: 'Metoro VPN',
+    image: 'metoro.png',
+    appStoreLink: 'https://apps.apple.com/us/app/metoro-vpn/id1495349422?ls=1',
+    appStoreImages: ['metoro-1.png', 'metoro-2.png'],
+    features: ['VPN', 'OpenVPN', 'IKEv2'],
     description:
-      "It is the best proxy VPN client for WiFi hotspot security and privacy protection. It is a free and unlimited VPN. Metoro VPN - It brings a high VPN speed and encrypted VPN connection to your iPhone.",
+      'It is the best proxy VPN client for WiFi hotspot security and privacy protection. It is a free and unlimited VPN. Metoro VPN - It brings a high VPN speed and encrypted VPN connection to your iPhone.',
   },
   {
-    name: "ESAR",
-    image: "esar.png",
+    name: 'ESAR',
+    image: 'esar.png',
     appStoreLink:
-      "https://apps.apple.com/us/app/esar-%D8%A7%D8%B3%D8%AA%D8%A7%D8%AC%D8%B1-%D8%A8%D8%B0%D9%83%D8%A7%D8%A1-%D8%A7%D9%8A%D8%B3%D8%A7%D8%B1/id1393026934",
-    appStoreImages: ["esar-1.jpg", "esar-2.jpg"],
-    features: ["GPS", "Google Maps", "Push Notifications"],
+      'https://apps.apple.com/us/app/esar-%D8%A7%D8%B3%D8%AA%D8%A7%D8%AC%D8%B1-%D8%A8%D8%B0%D9%83%D8%A7%D8%A1-%D8%A7%D9%8A%D8%B3%D8%A7%D8%B1/id1393026934',
+    appStoreImages: ['esar-1.jpg', 'esar-2.jpg'],
+    features: ['GPS', 'Google Maps', 'Push Notifications'],
     description:
-      "What AirBNB is for accommodations - ESAR is for cars. Renting Cars has never been so easy.",
+      'What AirBNB is for accommodations - ESAR is for cars. Renting Cars has never been so easy.',
   },
   {
-    name: "Insite by Investa",
-    image: "insite.png",
+    name: 'Insite by Investa',
+    image: 'insite.png',
     appStoreLink:
-      "https://apps.apple.com/us/app/insite-by-investa/id1478744000",
-    appStoreImages: ["insite-1.png", "insite-2.png"],
-    features: ["Bluetooth / Beacons", "Push Notifications", "Core Animation"],
+      'https://apps.apple.com/us/app/insite-by-investa/id1478744000',
+    appStoreImages: ['insite-1.png', 'insite-2.png'],
+    features: ['Bluetooth / Beacons', 'Push Notifications', 'Core Animation'],
     description:
-      "The Insite app by Investa provides a seamless, intuitive and enjoyable mobile experience that incorporates the latest in emerging building technologies. Insite allows occupants to navigate their workplace, view end of trip facilities or a meeting room, RSVP to Events and connect with our 5-star concierge services.",
+      'The Insite app by Investa provides a seamless, intuitive and enjoyable mobile experience that incorporates the latest in emerging building technologies. Insite allows occupants to navigate their workplace, view end of trip facilities or a meeting room, RSVP to Events and connect with our 5-star concierge services.',
   },
   {
-    name: "Flask for Axon",
-    image: "flask.png",
-    appStoreLink: "https://apps.apple.com/us/app/flask-for-axon/id1160464791",
-    appStoreImages: ["flask-1.png", "flask-2.png"],
-    features: ["ResearchKit", "HealthKit"],
+    name: 'Flask for Axon',
+    image: 'flask.png',
+    appStoreLink: 'https://apps.apple.com/us/app/flask-for-axon/id1160464791',
+    appStoreImages: ['flask-1.png', 'flask-2.png'],
+    features: ['ResearchKit', 'HealthKit'],
     description:
-      "The Axon SmartStudy by Medable system allows a researcher to build a study, create a mobile application associated with it and deploy to the Apple app store to start collecting data in less than 2 weeks. This application serves as a test bed for the various features and functions of the Axon SmartStudy system. A researcher can configure their study, tasks, and steps and visualize them in this app. Researchers can also use Axon SmartStudy to access Health data that is collected by your mobile device.",
+      'The Axon SmartStudy by Medable system allows a researcher to build a study, create a mobile application associated with it and deploy to the Apple app store to start collecting data in less than 2 weeks. This application serves as a test bed for the various features and functions of the Axon SmartStudy system. A researcher can configure their study, tasks, and steps and visualize them in this app. Researchers can also use Axon SmartStudy to access Health data that is collected by your mobile device.',
   },
   {
-    name: "Milo Pets",
-    image: "milo.png",
-    appStoreLink: "https://apps.apple.com/us/app/id1447288821",
-    appStoreImages: ["milo-1.png", "milo-2.png"],
-    features: ["Chat", "Push Notifications", "Web Sockets"],
+    name: 'Milo Pets',
+    image: 'milo.png',
+    appStoreLink: 'https://apps.apple.com/us/app/id1447288821',
+    appStoreImages: ['milo-1.png', 'milo-2.png'],
+    features: ['Chat', 'Push Notifications', 'Web Sockets'],
     description:
-      "Milo is a wellness membership dedicated to keeping your pet healthy. Members make simple monthly payments and receive best-in-class preventative care designed by your vet’s doctors and modeled after AAHA recommended guidelines.",
+      'Milo is a wellness membership dedicated to keeping your pet healthy. Members make simple monthly payments and receive best-in-class preventative care designed by your vet’s doctors and modeled after AAHA recommended guidelines.',
   },
   {
-    name: "Trainerize",
-    image: "trainerize.png",
+    name: 'Trainerize',
+    image: 'trainerize.png',
     appStoreLink:
-      "https://apps.apple.com/us/app/fitness-app-by-trainerize/id516851502",
-    appStoreImages: ["trainerize-1.png", "trainerize-2.png"],
-    features: ["React Native", "RxJS", "Native Modules"],
+      'https://apps.apple.com/us/app/fitness-app-by-trainerize/id516851502',
+    appStoreImages: ['trainerize-1.png', 'trainerize-2.png'],
+    features: ['React Native', 'RxJS', 'Native Modules'],
     description:
-      "Trainerize is an online personal training platform that empowers fitness professionals and studios to better connect to their clients when training them online or in-person.",
+      'Trainerize is an online personal training platform that empowers fitness professionals and studios to better connect to their clients when training them online or in-person.',
   },
   {
-    name: "LogTen Pro",
-    image: "logten-pro.png",
-    appStoreLink: "https://apps.apple.com/us/app/logten-pro/id837274884",
-    appStoreImages: ["logten-pro-1.png", "logten-pro-2.png"],
-    features: ["macOS/iOS", "React", "Parse", "Core Data"],
+    name: 'LogTen Pro',
+    image: 'logten-pro.png',
+    appStoreLink: 'https://apps.apple.com/us/app/logten-pro/id837274884',
+    appStoreImages: ['logten-pro-1.png', 'logten-pro-2.png'],
+    features: ['macOS/iOS', 'React', 'Parse', 'Core Data'],
     description:
-      "LogTen Pro is the most advanced pilot logbook for iOS, used by over 100,000 pilots world wide to track flight time, currency, endorsements, duty, rest, BFRs and more.",
+      'LogTen Pro is the most advanced pilot logbook for iOS, used by over 100,000 pilots world wide to track flight time, currency, endorsements, duty, rest, BFRs and more.',
   },
   {
-    name: "Human Dx",
-    image: "humandx.png",
-    appStoreLink: "https://apps.apple.com/us/app/human-dx/id1032495132",
-    appStoreImages: ["humandx-1.jpg", "humandx-2.jpg"],
+    name: 'Human Dx',
+    image: 'humandx.png',
+    appStoreLink: 'https://apps.apple.com/us/app/human-dx/id1032495132',
+    appStoreImages: ['humandx-1.jpg', 'humandx-2.jpg'],
     features: [
-      "React Native Chat",
-      "Natural Language Processing",
-      "Machine Learning",
+      'React Native Chat',
+      'Natural Language Processing',
+      'Machine Learning',
     ],
     description:
-      "Human Dx helps you develop your clinical reasoning with fast and fun clinical cases. Use our peer-reviewed cases to test your skills in less than five minutes each day, while learning from detailed teaching points and input from top medical professionals. Join a global community committed to learning and improving their clinical practice.",
+      'Human Dx helps you develop your clinical reasoning with fast and fun clinical cases. Use our peer-reviewed cases to test your skills in less than five minutes each day, while learning from detailed teaching points and input from top medical professionals. Join a global community committed to learning and improving their clinical practice.',
   },
   {
-    name: "Oviva",
-    image: "oviva.png",
-    appStoreLink: "https://apps.apple.com/us/app/oviva/id899484812",
-    appStoreImages: ["oviva-1.png", "oviva-2.png"],
-    features: ["Bluetooth", "Core Data"],
+    name: 'Oviva',
+    image: 'oviva.png',
+    appStoreLink: 'https://apps.apple.com/us/app/oviva/id899484812',
+    appStoreImages: ['oviva-1.png', 'oviva-2.png'],
+    features: ['Bluetooth', 'Core Data'],
     description:
-      "This is an app for Oviva patients only, which you can access with your patient credentials once you are referred to Oviva by your physician.",
+      'This is an app for Oviva patients only, which you can access with your patient credentials once you are referred to Oviva by your physician.',
   },
   {
-    name: "Instamic",
-    image: "instamic.png",
-    appStoreLink: "https://apps.apple.com/us/app/instamic-remote/id1141162465",
-    appStoreImages: ["instamic-1.png", "instamic-2.png"],
-    features: ["Bluetooth", "Voice Recording", "Low-level protocols", "OPX"],
+    name: 'Instamic',
+    image: 'instamic.png',
+    appStoreLink: 'https://apps.apple.com/us/app/instamic-remote/id1141162465',
+    appStoreImages: ['instamic-1.png', 'instamic-2.png'],
+    features: ['Bluetooth', 'Voice Recording', 'Low-level protocols', 'OPX'],
     description:
-      "Control your Instamic remotely to capture the sound you love! Instamic Remote transforms your iPhone, iPad or iPod Touch into a suitable remote control for your Instamic.",
+      'Control your Instamic remotely to capture the sound you love! Instamic Remote transforms your iPhone, iPad or iPod Touch into a suitable remote control for your Instamic.',
   },
   {
-    name: "SAFE",
-    image: "safe.png",
-    appStoreLink: "https://apps.apple.com/us/app/safe-app/id1154398105",
-    appStoreImages: ["safe-1.png", "safe-2.png"],
-    features: ["HealthKit", "Push Notifications"],
+    name: 'SAFE',
+    image: 'safe.png',
+    appStoreLink: 'https://apps.apple.com/us/app/safe-app/id1154398105',
+    appStoreImages: ['safe-1.png', 'safe-2.png'],
+    features: ['HealthKit', 'Push Notifications'],
     description:
-      "SAFE App lets you privately show your verified STD status on your phone, and confirm the status of your partners. It’s also the easiest and least expensive way to get tested for STDs.",
+      'SAFE App lets you privately show your verified STD status on your phone, and confirm the status of your partners. It’s also the easiest and least expensive way to get tested for STDs.',
   },
 ];
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   fillApps();
 });
 
 function fillApps() {
-  const features = document.querySelector("#apps");
+  const features = document.querySelector('#apps');
 
-  features.innerHTML = "";
+  features.innerHTML = '';
 
   for (let i = 0; i < apps.length; i++) {
     const app = apps[i];
 
     // div
-    const divElement = document.createElement("div");
-    divElement.className = "col-md-6 col-lg-2";
+    const divElement = document.createElement('div');
+    divElement.className = 'col-md-6 col-lg-2';
 
     // a
-    const anchor = document.createElement("a");
-    anchor.className = "portfolio-item d-block mx-auto popup-with-move-anim";
-    anchor.href = "#details-lightbox";
-    anchor.addEventListener("click", () => {
+    const anchor = document.createElement('a');
+    anchor.className = 'portfolio-item d-block mx-auto popup-with-move-anim';
+    anchor.href = '#details-lightbox';
+    anchor.addEventListener('click', () => {
       fillAppDetails(apps[i]);
     });
 
     // img
-    const image = document.createElement("img");
-    image.className = "img-fluid img-thumbnail round-xlarge";
+    const image = document.createElement('img');
+    image.className = 'img-fluid img-thumbnail round-xlarge';
     image.src = `images/portfolio/${app.image}`;
     image.alt == app.name;
 
@@ -175,52 +184,52 @@ function fillApps() {
     features.appendChild(divElement);
   }
 
-  $(".popup-with-move-anim").magnificPopup({
-    type: "inline",
+  $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
     fixedContentPos: false,
     fixedBgPos: true,
-    overflowY: "auto",
+    overflowY: 'auto',
     closeBtnInside: true,
     preloader: false,
     midClick: true,
     removalDelay: 300,
-    mainClass: "my-mfp-slide-bottom",
+    mainClass: 'my-mfp-slide-bottom',
   });
 }
 
 function fillAppDetails(app) {
-  const appStoreImages = document.querySelector("#app-store-images");
-  appStoreImages.innerHTML = "";
+  const appStoreImages = document.querySelector('#app-store-images');
+  appStoreImages.innerHTML = '';
 
   for (let image of app.appStoreImages) {
-    const img = document.createElement("img");
+    const img = document.createElement('img');
     img.src = `images/portfolio/details/${image}`;
     img.alt = app.name;
-    img.className = "img-fluid border";
+    img.className = 'img-fluid border';
     appStoreImages.appendChild(img);
   }
 
-  const appTitle = document.querySelector("#app-title");
+  const appTitle = document.querySelector('#app-title');
   appTitle.textContent = app.name;
 
-  const appDescription = document.querySelector("#app-description");
+  const appDescription = document.querySelector('#app-description');
   appDescription.textContent = app.description;
 
-  const ul = document.querySelector("#app-features");
-  ul.innerHTML = "";
+  const ul = document.querySelector('#app-features');
+  ul.innerHTML = '';
 
-  const appStoreButton = document.querySelector("#app-app-store-button");
+  const appStoreButton = document.querySelector('#app-app-store-button');
   appStoreButton.href = app.appStoreLink;
 
   for (let feature of app.features) {
-    const li = document.createElement("li");
-    li.className = "media";
+    const li = document.createElement('li');
+    li.className = 'media';
 
-    const i = document.createElement("i");
-    i.className = "far fa-check-square";
+    const i = document.createElement('i');
+    i.className = 'far fa-check-square';
 
-    const div = document.createElement("div");
-    div.className = "media-body";
+    const div = document.createElement('div');
+    div.className = 'media-body';
     div.textContent = feature;
 
     li.appendChild(i);
